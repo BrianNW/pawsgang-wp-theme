84456c53ea385ff3c28eb17d8f14e514425a1378
Brian <briantechdom@gmail.com>
2024-12-30 17:34:21 -0800

    Add custom font Nunito

--- functions.php
+++ functions.php
@@ -151,13 +151,18 @@ function pawsgang_scripts() {
 add_action( 'wp_enqueue_scripts', 'pawsgang_scripts' );
 
 // Custom fonts
+// font-family: 'Nunito', sans-serif;
 
 function enqueue_custom_fonts() {
 	if(!is_admin()) {
 		wp_register_style('source_sans_pro', 'https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap');
-		wp_enqueue_style('source_sans_pro')''
+		wp_register_style('nunito', 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap');
+		
+		wp_enqueue_style('source_sans_pro');
+		wp_enqueue_style('nunito');
 	}
 }
+add_action('wo_enqueu_script', 'enqueue_custom_fonts');
 
 /**
  * Implement the Custom Header feature.
